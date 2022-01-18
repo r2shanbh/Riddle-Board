d470b4692521d1a80ff71170f0f1cc268b92428a
Raksha Shanbhag <r2shanbh@gmail.com>
2022-01-17 20:48:49 -0500

    initiate Repo

---
+++ Frontend/src/components/NewNoteCreate.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    // to empty new note block
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form>
+        <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        />
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Content"
+          rows="5"
+        />
+        <button onClick={submitNote}>+</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
